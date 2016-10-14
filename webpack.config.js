aab8521a672a43d2a64f164824fa5c33f441a7e0
manuelfernando <manuelcamaron@hotmail.com>
2016-10-13 19:49:28 -0500

    It was activated webpack

---
+++ webpack.config.js
@@ -0,0 +1,24 @@
+/* eslint-disable */
+var webpack = require('webpack');
+
+var config = {
+    // webpack options
+    devtool: 'source-map',
+    entry: './src/index.js',
+    module: {
+        loaders: [
+            { test: /\.json/, loader: 'json' },
+            { test: /\.js$/, exclude: /node_modules/, loader: 'babel-loader' }
+        ]
+    },
+    output: {
+        filename: 'pubnub-angular2.js'
+    },
+    plugins: [
+        new webpack.BannerPlugin(require('./package.json').version, {
+            raw: false, entryOnly: true,
+        }),
+    ],
+}
+
+module.exports = config;
