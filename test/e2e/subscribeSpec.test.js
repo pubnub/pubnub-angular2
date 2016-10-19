a9b17e45d11eba95d79b999cc9272e4268e2e189
manuelfernando <manuelcamaron@hotmail.com>
2016-10-19 17:13:22 -0500

    It was added the unit test for present and messenger

--- test/e2e/subscribeSpec.test.js
+++ test/e2e/subscribeSpec.test.js
@@ -4,33 +4,80 @@ describe('"#subscribe()', function () {
     var channelName = undefined;
     var stringMessage = "hey";
 
-    beforeEach(function(){
+    pubnubService = new window.pubnub.angular2Service();
 
-        pubnubService = new window.pubnub.angular2Service();
+    pubnubService.init(config.demo);
 
-        pubnubService.init(config.demo);
+    beforeEach(function () {
 
         channelName = getRandomChannel();
     });
 
-    this.timeout(10000);
+    // afterEach(function () {
+    //
+    //     pubnubService.unsubscribe({channels: [channelName]});
+    // });
 
-    describe("success and connect callback", function(){
+    this.timeout(20000);
 
-        it("should be invoked", function(done){
+    describe('success and connect callback', function () {
+
+        it('should be invoked', function (done) {
 
             pubnubService.addListener({
-                message: function(m){
+                message: function (m) {
 
                     expect(m.message).to.be.equal(stringMessage);
                     expect(m.channel).to.be.equal(channelName);
                     done();
+
                 }
             });
 
             pubnubService.subscribe({channels: [channelName]});
 
             pubnubService.publish({channel: channelName, message: stringMessage});
-        })
+        });
+    });
+
+    describe('presence callback', function () {
+
+        it('should be invoked', function (done) {
+
+            var uuid = 'blah';
+
+            pubnubService.getInstance('another').init(config.demo);
+
+            pubnubService.getInstance('another').setUUID(uuid);
+
+            pubnubService.getInstance('another').addListener({
+                presence: function (p) {
+
+                    expect(p.uuid).to.be.equal(uuid);
+                    done();
+                }
+            });
+
+            pubnubService.getInstance('another').subscribe({channels: [channelName], withPresence: true});
+        });
+
+    });
+
+    describe('Message callback', function(){
+
+        it('The message callback should be invoked as usual', function(done){
+
+            pubnubService.addListener({
+                message: function(m){
+
+                    expect(m.message).to.be.equal(stringMessage);
+                    done();
+                }
+            });
+
+            pubnubService.subscribe({'channels': [channelName]});
+
+            pubnubService.publish({channel: channelName, message: stringMessage});
+        });
     });
 });
