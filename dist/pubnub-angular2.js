fb662139e758ae6489f2b587efd5990e4aa1954f
Max Presman <max@pubnub.com>
2017-02-22 22:02:01 -0800

    update dist

--- dist/pubnub-angular2.js
+++ dist/pubnub-angular2.js
@@ -1,4 +1,4 @@
-/*! 1.0.0-beta.9 */
+/*! 1.0.0 */
 (function webpackUniversalModuleDefinition(root, factory) {
 	if(typeof exports === 'object' && typeof module === 'object')
 		module.exports = factory();
